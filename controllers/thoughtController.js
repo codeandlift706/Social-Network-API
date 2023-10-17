22e6662dc5154e314adc07033412ba9e1f064bac
Code Queen <channguyen97@gmail.com>
2023-10-16 20:05:25 -0700

    added get calls

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,11 +2,28 @@ const { Thought } = require('../models');
 
 module.exports = {
     async getAllThoughts(req, res) {
-
+        try {
+            const thoughts = await Thought.find();
+            res.json(thoughts);
+        } catch (err) {
+            res.status(500).json(err);
+        }
     },
+
     async getSingleThought(req, res) {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId });
 
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with that ID' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
     },
+
     async createThought(req, res) {
 
     },
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,11 +2,26 @@ const { User } = require('../models');
 
 module.exports = {
     async getAllUsers(req, res) {
-
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
     },
+
     async getSingleUser(req, res) {
-
+        try {
+            const user = await User.findOne({ _id: req.params.userId });
+            if (!user) {
+                return res.status(404).json({ message: 'No user with that ID' });
+            }
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
     },
+    
     async createUser(req, res) {
 
     },
